1fcdc19a4d3c0c5662fb4a2dc6f443a0de1e44d6
Shawn Gwin <shawn@gwins.net>
2020-03-06 20:32:53 -0500

    Added Intake and updated shooter

--- InfiniteRecharge2020/src/main/java/frc/robot/commands/IntakeCommand.java
+++ InfiniteRecharge2020/src/main/java/frc/robot/commands/IntakeCommand.java
@@ -7,50 +7,29 @@
 
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import frc.robot.subsystems.ChuteSubSystem;
-import frc.robot.subsystems.ShooterSubSystem;
 
-public class ShooterCommand extends CommandBase {
-  private final ShooterSubSystem shooterSubSystem;
-  private final ChuteSubSystem chuteSubSystem;
-  private double timerDelay = 5.0;
-  private Timer timer = new Timer();
+public class IntakeCommand extends CommandBase {
   /**
-   * Creates a new ShooterCommand.
+   * Creates a new IntakeCommand.
    */
-  public ShooterCommand(ShooterSubSystem shooterSubsystem, ChuteSubSystem chuteSubsystem) {
+  public IntakeCommand() {
     // Use addRequirements() here to declare subsystem dependencies.
-    shooterSubSystem = shooterSubsystem;
-    chuteSubSystem = chuteSubsystem;
-    addRequirements(shooterSubsystem);
-    addRequirements(chuteSubsystem);
   }
 
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    timer.start();
-    shooterSubSystem.startMotors();
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    if(timer.get() >= timerDelay )
-      {
-        //Run the chute
-        chuteSubSystem.upMotor();
-      }
-    //shooterSubSystem.showValues();
   }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-    shooterSubSystem.stopMotors();
-    chuteSubSystem.stopMotor();
   }
 
   // Returns true when the command should end.
